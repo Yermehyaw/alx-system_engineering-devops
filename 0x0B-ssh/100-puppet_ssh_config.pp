b9cde4aa43be258066aaa609caaf681afd53fbf4
Yermehyaw <yermehyaw@gmail.com>
2024-07-01 17:18:56 -0700

    Use puppet to edit ssh config file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+# Puppet file to configure client ssh_config file
+
+class ssh_config {
+      $config_content = @(EOT)
+      		      Host *
+      		      	   HostName 35.153.52.184
+		      	   Port 22
+		      	   User ubuntu
+		      	   PasswordAuthentication no
+		      	   IdentityFile ~/.ssh/school
+	EOT
+
+	file { '~/.ssh/ssh_config':
+            ensure => file,
+      	    owner => 'root',
+      	    group => 'root',
+      	    mode => '755',
+      	    content => $config_content
+      }
+}
+
+include ssh_config
