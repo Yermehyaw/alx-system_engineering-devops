3cffccb2918f5539be928a152fd84d6cb4414636
Yermehyaw <yermehyaw@gmail.com>
2024-07-30 17:48:57 -0700

    Fix csv file prod with csv module

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,10 +3,14 @@
 """
 Modules Imported: requests
 
+csv: create csv formatted text
 requests: make HTTP requests and responses in py script
+subprocess: execute shell commands from py script
 sys: execute shell commands from py script
 """
+import csv
 import requests
+import subprocess
 import sys
 
 
@@ -25,15 +29,15 @@ def get_cv(employee_id):
     except Exception:
         raise TypeError("Enter a valid argument")
 
-    # Get employee name
+    # Get employee name abd username
     try:
         r = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    except requests.exeception.RequestException:
+    except requests.execeptions.RequestException:
         return "Conection failed"
     response = requests.get(r)
     json_obj = response.json()  # a dict
-    employee_name = json_obj.get('name')  # no need to make an extra var
-    print(employee_name)
+    employee_name = json_obj.get('name')
+    employee_username = json_obj.get['usernane']
 
     # get the to-do list
     r = f"https://jsonplaceholder.typicode.com/todos/?userId={employee_id}"
@@ -41,41 +45,41 @@ def get_cv(employee_id):
         json_obj = response.json()  # list of dicts
         print(json_obj)
         total = len(json_obj)  # only user specific tasks are in response
-        tasks_completed = 0
+ #       tasks_completed = 0
         tasks_title = []
         for i in range(total):
             for key, value in json_obj[i].items():
                 # if key and value corresponds to 'completed: true'
-                if key == 'completed' and value:
-                    tasks_completed += 1
-                    tasks_title.append(json_obj[i].get('title'))
-    result = (f"Employee {employee_name} is done with\
-            ({tasks_completed}/{total})")
-    print(result)
-    for i in range(tasks_completed):
-        print(f"\t {tasks_title[i]}")
-    print(tasks_title)
-    return result
+                if key == 'completed':
+#                    tasks_completed += 1
+                    task_status = value
+                    tasks_title.append(json_obj[i].get('title'))  # list of dict
+#    for i in range(tasks_completed):
+#        print(f"\t {tasks_title[i]}")
 
 
 # Get employee details in csv
-r = f'https://jsonplaceholder.typicode.com/todos/?userId={employee_id}'
-with requests.get(r) as response:
-    json_obj = response.json()
-
-    result = []
-    for i in range():
-        result = result.append(f'"{employee_id}", 
-                            "{username}", 
-                            "{task_status}", 
+    result = [[]]
+    for i in range(total):
+        result = result[0].append(f'"{employee_id}",
+                            "{employee_username}",
+                            "{str(task_status)}",
                             "{task_title[i]}"'
                 )
+    return result
 
 
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    csv = get_csv(employee_id)  # csv is a list
-    sys.cmd('touch {employee_id}.csv')
-    with open(f'{employee_id}.csv') as f:
-        f.write(str(csv))
+    csv_data = get_csv(employee_id)  # csv is a list of lists
+
+    create_csv_cmd = f'touch {employee_id}.csv'
+    try:
+        subprocess.run(create_csv_cmd, shell=True, check=True)
+    except CalledProcessError:
+        print('Couldnt create csv file')
+
+    with open(f'{employee_id}.csv', 'w', newline='') as f:
+        write_in = csv.write(f)
+        write_in.write(csv_data)
