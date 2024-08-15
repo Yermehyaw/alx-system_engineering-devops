7ba2ef9ed15a73604aaa21b372392765131f226a
Yermehyaw <yermehyaw@gmail.com>
2024-08-15 15:21:56 +0100

    Convert to a func

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """
-Queries a subreddit and returns the no of subscribers
-
+Queries a Reddit API
 
 IMPORTS: requests
-
 requests: Send and receive data packets from a URI
 """
 import requests
 
 
+def number_of_subscribers(subreddit):
+    """
+    Queries a valid subreddit and retrives the no of subs
+
+    Args:
+    subreddit(str): A reddit topic
+
+    Return:
+    No of subs a subreddit has
+    """
 url = ''
 with requests.get(url, follow_redirects=false) as response:
     resp_json = response.json()
